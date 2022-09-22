8541ed679592b1c7729df4cd34e45be303513770
Felipe <Felipe.orivas@gmail.com>
2022-09-21 22:58:23 -0300

    get eventos service added

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -2,9 +2,12 @@ const { response } = require('express')
 const Evento = require('../models/Evento')
 
 const getEventos = async (req, res = response) => {
+
+    const eventos = await Evento.find().populate('user', 'name')
+
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
     })
 }
 
